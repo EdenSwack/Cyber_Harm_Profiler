97fe315d94b8fedf0e93be5ba4e88f0f4e9b785d
EdenSwack <93931254+EdenSwack@users.noreply.github.com>
2024-06-04 17:23:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Reddit API credentials
+reddit_client_id = 'YOUR_REDDIT_CLIENT_ID'
+reddit_client_secret = 'YOUR_REDDIT_CLIENT_SECRET'
+reddit_user_agent = 'YOUR_REDDIT_USER_AGENT'
+reddit_username = 'YOUR_REDDIT_USERNAME'
+
+# Twitter API credentials
+twitter_api_key = 'YOUR_TWITTER_API_KEY'
+twitter_api_host = 'YOUR_TWITTER_API_HOST'
