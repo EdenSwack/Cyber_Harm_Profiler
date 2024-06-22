0807feaff78c1c0482fc2c113c1ba429b1e89919
EdenSwack <93931254+EdenSwack@users.noreply.github.com>
2024-06-22 17:23:51 +0300

    Update reddit_scraper.py

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -2,8 +2,17 @@ import praw
 import prawcore
 import re
 import tempfile
+from config import client_id, client_secret, user_agent, username
 
-def scrape_subreddits(reddit, target_tags, n):
+
+def scrape_subreddits(target_tags, n):
+    reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    username=username,
+    check_for_async=False)
+    
     harmful_subreddits = []
     for tag in target_tags:
         for subreddit in reddit.subreddits.search('flair_name:"' + tag + '"', limit=None):
@@ -20,10 +29,12 @@ def scrape_subreddits(reddit, target_tags, n):
                     subreddits_saved += 1
             except (prawcore.exceptions.Forbidden, prawcore.exceptions.NotFound, prawcore.exceptions.RequestException) as e:
                 print(f"Error accessing subreddit '{submission.title}': {e}")
+    with open(file_path, 'r') as file:
+    content = file.read()
 
-    return file_path
+    return content
 
-def extract_twitter_handles(file_path):
+def extract_twitter_handles(content):
     with open(file_path, 'r') as file:
         content = file.read()
 
